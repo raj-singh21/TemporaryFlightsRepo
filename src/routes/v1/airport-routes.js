a424784d7f1aa08465679f01879ff56748f01c97
Raj Singh <rocksraj628@gmail.com>
2024-02-21 11:15:05 +0530

    Added airport resource crud

---
+++ src/routes/v1/airport-routes.js
@@ -0,0 +1,24 @@
+const express= require('express');
+
+const{AirportController}=require('../../controllers');
+const {AirportMiddlewares}=require('../../middlewares')
+
+const router=express.Router();
+
+// /api/v1/airplanes POST
+router.post('/',
+        AirportMiddlewares.validateCreateRequest,
+        AirportController.createAirport);
+
+// /api/v1/airplanes GET
+router.get('/',
+        AirportController.getAirports);
+
+router.get('/:id', 
+        AirportController.getAirports);
+
+// /api/v1/airplanes DELETE
+router.delete('/:id', 
+        AirportController.destroyAirport);
+
+module.exports=router;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,6 +5,7 @@ const{InfoController}=require('../../controllers');
 const airplaneRoutes = require('./airplane-routes');
 
 const cityRoutes=require('./city-routes');
+const airportRoutes=require('./airport-routes');
 
 
 const router=express.Router();
@@ -12,6 +13,8 @@ const router=express.Router();
 router.use('/airplanes',airplaneRoutes);
 
 router.use('/cities',cityRoutes);
+
+router.use('/airports',airportRoutes);
 
 
 router.get('/info', InfoController.info);
